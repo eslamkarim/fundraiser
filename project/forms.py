64127c6eb2d7ea675196fc83b7080bea71d3853f
Eslam karim gaber <eslamkarim222@gmail.com>
2020-03-18 20:53:07 +0200

    finishing variable project page with donations and target title and details finished
    clean some custom js code and css

---
+++ project/forms.py
@@ -0,0 +1,74 @@
+from django import forms
+
+from .models import Category, Tag
+import datetime
+
+class DateInput(forms.DateInput):
+      input_type = 'date'
+
+class ContactForm(forms.Form):
+      project_title = forms.CharField(max_length=100, label='Project Title')
+      details = forms.CharField(label='Project Details',widget=forms.Textarea)
+      category = forms.ModelChoiceField(queryset=Category.objects.all(),required=True)
+      target = forms.IntegerField(min_value=1,max_value=1000000000)
+      tags = forms.ModelMultipleChoiceField(widget = forms.CheckboxSelectMultiple,queryset=Tag.objects.all())
+      start_date = forms.DateField(widget=DateInput,initial=datetime.date.today,required=True)
+      end_date = forms.DateField(widget=DateInput,initial=datetime.date.today,required=True)
+      
+      
+      def clean_project_title(self):
+            project_title = self.cleaned_data['project_title']
+            if len(project_title) == 0:
+                  raise forms.ValidationError("project title can't be empty ")
+            if len(project_title) >100:
+                  raise forms.ValidationError("project title can't be more than 100 character ")
+            return project_title
+      
+      def clean_details(self):
+            details = self.cleaned_data['details']
+            if len(details) == 0:
+                  raise forms.ValidationError("project title can't be empty ")
+            return details
+      
+      def clean_category(self):
+            category = self.cleaned_data['category']
+            try:
+                  Category.objects.get(name = category)
+            except:
+                  raise forms.ValidationError("please choose from list")
+            return category
+      
+      def clean_target(self):
+            target = self.cleaned_data['target']
+            if target <= 0:
+                  raise forms.ValidationError("project title can't lower than 1 ")
+            if target > 1000000000:
+                  raise forms.ValidationError("project title can't be more than 1000000000 ")
+            return target
+      
+      def clean_tags(self):
+            tags = self.cleaned_data['tags']
+            for val in tags:
+                  try:
+                        Tag.objects.get(name=val)
+                  except:
+                        raise forms.ValidationError("please choose from list")
+            return tags
+                  
+      
+      def clean_start_date(self):
+            start_date = self.cleaned_data['start_date']
+            print("Date = ",start_date)
+            if (start_date-datetime.date.today()).days < 0:
+                  raise forms.ValidationError("Start date can't be in the past")
+            return start_date
+
+      def clean_end_date(self):
+            end_date = self.cleaned_data['end_date']
+            if self.cleaned_data.get('start_date')==None:
+                  return end_date
+            start_date = self.cleaned_data['start_date']
+            print("Date = ",end_date)
+            if (end_date-start_date).days <= 0:
+                  raise forms.ValidationError("end date must be after start Date")
+            return end_date
--- project/views.py
+++ project/views.py
@@ -3,6 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def project(request):
     context = {
-        "images": ["https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXzoLYuz49nb6hd68DgWT7zFBxe5E81268lhh94dIMnyzRzNQM","https://i.insider.com/5d02563ddaa4821bf4575092?width=1100&format=jpeg&auto=webp","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXzoLYuz49nb6hd68DgWT7zFBxe5E81268lhh94dIMnyzRzNQM"]
+        "images": ["https://icatcare.org/app/uploads/2018/06/Layer-1704-1920x840.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXzoLYuz49nb6hd68DgWT7zFBxe5E81268lhh94dIMnyzRzNQM","https://i.insider.com/5d02563ddaa4821bf4575092?width=1100&format=jpeg&auto=webp","https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXzoLYuz49nb6hd68DgWT7zFBxe5E81268lhh94dIMnyzRzNQM"],
+        "title": "this is a title",
+        "details":
+"""ou have 17 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
+Run python manage.py migrate to apply them.
+March 18, 2020 - 13:34:36
+Django version 3.0.4, using settings fundraiser.settings
+Quit the server with CTRL-BREAK.
+Watching for file changes with StatReloader
+Performing system checks...
+System check identified no issues (0 silenced).
+You have 17 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
+Run python manage.py migrate to apply them.
+March 18, 2020 - 13:34:39
+Django version 3.0.4, using settings fundraiser.settings""",
+      "current":9000,
+      "target":10000
     }
     return render(request,'project/project.html',context)
