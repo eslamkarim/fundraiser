3e3120b1119a887d4797559f7c2b2207fa133a98
AymanMagdy <magdia883@gmail.com>
2020-03-19 22:06:35 +0200

    edit registeration validations on js

--- static/js/sign_up_validator.js
+++ static/js/sign_up_validator.js
@@ -3,28 +3,31 @@ document.getElementById("sign_up_btn").addEventListener("click", sign_up);
 function sign_up(){
     first_name = document.getElementById("first_name").value
     last_name = document.getElementById("last_name").value
-    phone_numebr = document.getElementById("phone_number").value
+    phone_number = document.getElementById("phone_number").value
     email_address = document.getElementById("email").value
-    passowrd = document.getElementById("password").value
+    password = document.getElementById("password").value
     conf_password = document.getElementById("conf_password").value
 
-    if (validate_email(email_address) && validate_phone_numebr(phone_numebr) && is_pasword_matching(passowrd, conf_password)){
-        new_user = {
-            first_name: this.first_name,
-            last_name: this.last_name,
-            phone_numebr: this.phone_numebr,
-            email_address: this.email_address,
-            passowrd: this.passowrd,
-            conf_password: this.conf_password
-        }
-
-        console.log("Done sign up")
-        return new_user
-    } else {
-        console.log();
+    if (first_name.length == 0 || last_name.length == 0 || phone_number.length == 0 || email_address.length == 0 || password == 0 || conf_password == 0 ){
         return (false)
-    }
+    } else {
+            if (validate_email(email_address) && validate_phone_number(phone_number) && is_password_matching(password, conf_password)){
+                new_user = {
+                    first_name: this.first_name,
+                    last_name: this.last_name,
+                    phone_number: this.phone_number,
+                    email_address: this.email_address,
+                    password: this.password,
+                    conf_password: this.conf_password
+                }
 
+                window.alert("Check your email to activate your account.")
+                return new_user
+            } else {
+                window.alert("Invalid inputs")
+                return (false)
+            }
+    }
 }
 
 function validate_email(email_address){
@@ -34,16 +37,16 @@ function validate_email(email_address){
         return (false)
 }
 
-function validate_phone_numebr(phone_numebr){
-    if (/^(010|011|015|012)([0-9]{8})/.test(phone_numebr))
+function validate_phone_number(phone_number){
+    if (/^(010|011|015|012)([0-9]{8})/.test(phone_number))
         return (true)
     else 
         return (false)
 
 }
 
-function is_pasword_matching(passowrd, conf_password){
-    if (passowrd == conf_password)
+function is_password_matching(password, conf_password){
+    if (password == conf_password)
         return true
     else 
         return false
