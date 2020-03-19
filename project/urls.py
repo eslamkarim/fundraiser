40819b597efd732e7865bbfe1909e63e683f16fb
MinaMaher0 <minamaher30697@gmail.com>
2020-03-19 19:51:30 +0200

    donate button

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
-from .views import create,project
+from .views import create, project, donate
 
 urlpatterns = [
-    path('create/', create,name='create'),
+    path('create/', create, name='create'),
     path('', project),
+    path('<int:project_id>/donate/',donate, name='donate'),
 ]
