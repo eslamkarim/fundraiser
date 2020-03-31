64537ca265656ad316e1e0aadbce79a3ad51f536
samar <samar@localhost.localdomain>
2020-03-31 22:04:57 +0200

    user views

--- user-profile/views.py
+++ user-profile/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-# import for models
+from random import shuffle
+from project.models import Project_data, Category, project_tags,  Project_pics
+from django.contrib.auth.models import User
 
 # Create your views here.
+#
+# def userProfile(request):#id
+#  #   user=Users.objects.get(id=id)
+#     user={"id":1,"name":"samar mahmoud","phone":"011455","mail":"samar@yahoo.com","country":"Egypt","facebook":"www.facebook.com"}
+#     args={"data":user}
+#     return render(request,'user-profile/user-profile.html',args)
 
-def userProfile(request):#id
- #   user=Users.objects.get(id=id)
-    user={"id":1,"name":"samar mahmoud","phone":"011455","mail":"samar@yahoo.com","country":"Egypt","facebook":"www.facebook.com"}
-    dic={"data":user}
-    return render(request,'user-profile/user-profile.html',dic)
+
+def userProject(request,id):
+    project = Project_data.objects.get(id=id)
+    # dic = {"p_data": project}
+    return render(request, "user-profile/user-profile.html", {"p_data": project})
+
+def userProfile (request,id):
+    user =User.objects.get(id=id)
+    # args ={'user':request.user}
+    return render(request,'user-profile/user-profile.html',{'user_data':user})
 
